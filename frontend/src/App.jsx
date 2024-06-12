23a2fca9f9a4d0b5d1bbdcbf3af6658966ca0f06
baothuyt <thuyttbps26235@fpt.edu.vn>
2024-06-12 08:20:30 +0700

    Add files via upload

---
+++ frontend/src/App.jsx
@@ -0,0 +1,24 @@
+import { BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import Layout from "./layouts/layout";
+import HomePages from "./pages/HomePages";
+// import Detail from "./components/Detail/Detail";
+function App() {
+  return (
+    <Router>
+    <Routes>
+      <Route path="/" element={<Layout>
+        <HomePages/>
+      </Layout>} />
+      <Route path="/search" element={<Layout>
+        <p>Search Page</p>
+      </Layout>} />
+      
+      <Route path="/home" element={<Layout>
+        <p>Trang Chủ</p>
+      </Layout>} />
+    </Routes>
+  </Router>
+  )
+}
+
+export default App
