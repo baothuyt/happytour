7913981cc336dd8fba860a285f007ab56d430e0f
baothuyt <thuyttbps26235@fpt.edu.vn>
2024-07-15 14:17:50 +0700

    Update Breadcrumbs.jsx

--- frontend/src/Breadcrumbs/Breadcrumbs.jsx
+++ frontend/src/Breadcrumbs/Breadcrumbs.jsx
@@ -1,11 +1,37 @@
 import React from "react";
 import useBreadcrumbs from 'use-react-router-breadcrumbs'
-const Breadcrumbs = () =>{
-    const breadcrumbs = useBreadcrumbs()
-    console.log(breadcrumbs)
+import { Link } from "react-router-dom";
+import { IoChevronForward } from 'react-icons/io5';
+//import { IoIoChevronForward} from 'react-icons/io'
+const Breadcrumbs = ({name, category}) =>{
+    // console.log(name, category)
+    const routes = [
+        {path: "/:category", breadcrumb: category},
+        {path: "/", breadcrumb: "Home"},
+        {path: "/:category/:tourId/:name", breadcrumb: name},
+        
+    ];
+    const breadcrumb = useBreadcrumbs(routes)
+    console.log(breadcrumb)
     return(
-        <span>Trang chủ &gt; Tour miền bắc &gt; HÀ NỘI - NINH BÌNH - HẠ LONG - YÊN TỬ - SA PA (đường cao tốc) - HÀ NỘI</span>
-
+        <>
+        
+        <div style={{ display: 'flex', alignItems: 'center'}}>
+            {breadcrumb?.filter(el => !el.match.route === false).map(({match, breadcrumb}, index, self) => (
+                <Link key={match.pathname} to={match.pathname}
+                 style={{display: 'flex', 
+                        alignItems: 'center', 
+                        textDecoration: 'none', 
+                        color:'black',
+                        '&:hover': {color: 'red'} 
+                        }}>
+                    <span>{breadcrumb}</span>
+                    {index !== self.length - 1 && <IoChevronForward style={{ marginLeft: '5px' }} />}
+                </Link>
+            ))}
+        </div>
+        </>
+        
     )
 }
 
