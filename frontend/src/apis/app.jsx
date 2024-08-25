a5c403a1dedf1da36a6324edc233817377e0d7e6
datbachan1 <hjhjhj2367@gmail.com>
2024-08-25 20:36:30 +0700

    update edit delete category

--- frontend/src/apis/app.jsx
+++ frontend/src/apis/app.jsx
@@ -3,4 +3,21 @@ import axios from "../axios";
 export const apiGetCategories = () => axios({
     url: '/api/tour-category',
     method: 'get'
-})
+});
+
+export const apiUpdateCategory = (id, data) => axios({
+    url: `/api/tour-category/${id}`,
+    method: 'put',
+    data
+});
+
+export const apiDeleteCategory = (id) => axios({
+    url: `/api/tour-category/${id}`,
+    method: 'delete'
+});
+
+export const apiAddCategory = () => axios({
+    url: '/api/tour-category',
+    method: 'post',
+   
+});
